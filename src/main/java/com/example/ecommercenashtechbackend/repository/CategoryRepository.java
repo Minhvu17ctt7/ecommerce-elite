f3c78582f818b7399c0cfd208704312a27bbe61f
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-04-24 10:54:14 +0700

    CRUD category

--- src/main/java/com/example/ecommercenashtechbackend/repository/CategoryRepository.java
+++ src/main/java/com/example/ecommercenashtechbackend/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.ecommercenashtechbackend.repository;
 
 import com.example.ecommercenashtechbackend.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByName(String name);
+
+    Page<Category> findByNameContaining(String name, Pageable pageable);
 }
--- src/main/java/com/example/ecommercenashtechbackend/service/CategoryService.java
+++ src/main/java/com/example/ecommercenashtechbackend/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.example.ecommercenashtechbackend.service;
 
 import com.example.ecommercenashtechbackend.dto.request.CategoryRequestDto;
+import com.example.ecommercenashtechbackend.dto.request.CategoryUpdateRequestDto;
 import com.example.ecommercenashtechbackend.entity.Category;
 
 import java.util.List;
@@ -8,5 +9,11 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getAllCategories();
 
-    Category saveCategory(CategoryRequestDto categoryRequestDto);
+    Category createCategory(CategoryRequestDto categoryRequestDto);
+
+    List<Category> getAllCategoriesPagination(int pageNumber, int pageSize, String sortField, String sortName, String keyword);
+
+    Category updateCategory(CategoryUpdateRequestDto categoryUpdateRequestDto);
+
+    void deleteCategory(Long id);
 }
