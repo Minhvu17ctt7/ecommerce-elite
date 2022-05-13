907d006b5ac71cbd2cd3961d6ab80aafa8790cf6
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-13 13:45:23 +0700

    FE ADMIN: fix async on upload image firebase

--- frontend/admin/src/firebase/upload.js
+++ frontend/admin/src/firebase/upload.js
@@ -1,16 +1,11 @@
 import storage from './index';
 
 const uploadImage = async (path, imageAsFile) => {
-    const uploadTask = storage.ref(`${path}/${imageAsFile.name}`).put(imageAsFile);
+    const uploadTask = storage.ref(`/${path}/${imageAsFile.name}`);
 
-    //initiates the firebase side uploading 
-    await uploadTask.on('state_changed',
-        (snapShot) => {
-        }, (err) => {
-            console.log(err)
-        })
-    let urlImage = await storage.ref('images').child(imageAsFile.name).getDownloadURL();
-    return urlImage;
+    await uploadTask.put(imageAsFile);
+    const url = await uploadTask.getDownloadURL();
+    return url;
 }
 
 export default uploadImage;
