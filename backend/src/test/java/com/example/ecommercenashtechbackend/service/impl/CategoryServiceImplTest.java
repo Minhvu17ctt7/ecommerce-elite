7bccfb7f4889e93715e25d7048204238e324ab4f
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-16 09:59:50 +0700

    BE: add test success in CategoryService test

--- backend/src/test/java/com/example/ecommercenashtechbackend/service/impl/CategoryServiceImplTest.java
+++ backend/src/test/java/com/example/ecommercenashtechbackend/service/impl/CategoryServiceImplTest.java
@@ -24,6 +24,7 @@ public class CategoryServiceImplTest {
     private Util util;
     private CategoryServiceImpl categoryService;
     private Category categoryInitial;
+    private Category categoryExpected;
     private CategoryRequestDto categoryRequestDto;
     private CategoryUpdateRequestDto categoryUpdateRequestDto;
 
@@ -36,6 +37,7 @@ public class CategoryServiceImplTest {
         categoryInitial = mock(Category.class);
         categoryRequestDto = mock(CategoryRequestDto.class);
         categoryUpdateRequestDto = mock(CategoryUpdateRequestDto.class);
+        categoryExpected = mock(Category.class);
     }
 
     @Test
@@ -53,5 +55,27 @@ public class CategoryServiceImplTest {
         ConflictException conflictException = assertThrows(ConflictException.class, () -> categoryService.updateCategory(categoryUpdateRequestDto));
         assertThat(conflictException.getMessage()).isEqualTo("Category name already exits");
     }
+
+    @Test
+    public void updateCategory_ShouldReturnCategory_WhenUpdateSuccessNoParent() {
+        Category categoryResult = mock(Category.class);
+        when(categoryRepository.findById(categoryUpdateRequestDto.getId())).thenReturn(Optional.ofNullable(categoryInitial));
+        when(categoryRepository.findByName(categoryUpdateRequestDto.getName())).thenReturn(Optional.ofNullable(null));
+        when(modelMapper.map(categoryUpdateRequestDto, Category.class)).thenReturn(categoryExpected);
+        when(categoryRepository.save(categoryExpected)).thenReturn(categoryResult);
+        Category result = categoryService.updateCategory(categoryUpdateRequestDto);
+        assertThat(result).isEqualTo(categoryResult);
+    }
+
+//    @Test
+//    public void updateCategory_ShouldThrownNotFoundException_WhenFindCategoryParentByIdNotFound() {
+//        CategoryUpdateRequestDto categoryUpdateRequest = CategoryUpdateRequestDto.builder().parentId(1L).build();
+//        when(categoryRepository.findById(categoryUpdateRequest.getId())).thenReturn(Optional.ofNullable(categoryInitial));
+//        when(categoryRepository.findByName(categoryUpdateRequest.getName())).thenReturn(Optional.ofNullable(null));
+//        when(modelMapper.map(categoryUpdateRequest, Category.class)).thenReturn(categoryExpected);
+//        when(categoryRepository.findById(categoryUpdateRequest.getId())).thenReturn(Optional.ofNullable(null));
+//        NotFoundException notFoundException = assertThrows(NotFoundException.class, () -> categoryService.updateCategory(categoryUpdateRequest));
+//        assertThat(notFoundException.getMessage()).isEqualTo("Parent category not found");
+//    }
 
 }
