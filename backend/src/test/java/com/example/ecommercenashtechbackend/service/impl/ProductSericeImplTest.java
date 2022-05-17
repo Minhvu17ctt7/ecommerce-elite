4dfab7f66f3b6e86af5f18f099cbe38384f155d2
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-17 09:14:24 +0700

    BE: test method updateProduct in ProductServiceImpl

--- backend/src/test/java/com/example/ecommercenashtechbackend/service/impl/ProductSericeImplTest.java
+++ backend/src/test/java/com/example/ecommercenashtechbackend/service/impl/ProductSericeImplTest.java
@@ -1,6 +1,7 @@
 package com.example.ecommercenashtechbackend.service.impl;
 
 import com.example.ecommercenashtechbackend.dto.request.ProductCreateRequestDto;
+import com.example.ecommercenashtechbackend.dto.request.ProductUpdateRequestDto;
 import com.example.ecommercenashtechbackend.dto.response.ProductResponseDto;
 import com.example.ecommercenashtechbackend.entity.Category;
 import com.example.ecommercenashtechbackend.entity.Product;
@@ -29,28 +30,28 @@ public class ProductSericeImplTest {
     private Util util;
     private Product productInitial;
     private ProductCreateRequestDto productCreateRequestDto;
+    private ProductUpdateRequestDto productUpdateRequestDto;
     private Product productExpected;
     private Category category;
     private ProductResponseDto productResponseDto;
 
     @BeforeEach
     public void setUp() {
-
         productRepository = mock(ProductRepository.class);
         categoryRepository = mock(CategoryRepository.class);
         modelMapper = mock(ModelMapper.class);
         util = mock(Util.class);
         productInitial = mock(Product.class);
         productCreateRequestDto = mock(ProductCreateRequestDto.class);
+        productUpdateRequestDto = mock(ProductUpdateRequestDto.class);
         productServiceImpl = new ProductServiceImpl(productRepository, categoryRepository, modelMapper, util);
         productExpected = mock(Product.class);
         category = mock(Category.class);
         productResponseDto = mock(ProductResponseDto.class);
-
     }
 
     @Test
-    public void createProduct_ShouldThrownConflictException_WhenFindProductByNameExist() {
+    public void createProduct_ShouldThrownConflictException_WhenFindProductByNameNotExist() {
         when(productRepository.findByName(productCreateRequestDto.getName())).thenReturn(Optional.ofNullable(productExpected));
         ConflictException conflictException = assertThrows(ConflictException.class, () -> productServiceImpl.createProduct(productCreateRequestDto));
         assertThat(conflictException.getMessage()).isEqualTo("Product name already exits");
@@ -73,6 +74,49 @@ public class ProductSericeImplTest {
         when(productRepository.save(productExpected)).thenReturn(productExpected);
         when(modelMapper.map(productExpected, ProductResponseDto.class)).thenReturn(productResponseDto);
         ProductResponseDto result = productServiceImpl.createProduct(productCreateRequestDto);
+        assertThat(result).isEqualTo(productResponseDto);
+    }
+
+    @Test
+    public void updateProduct_ShouldThrownNotFoundException_WhenFindProductByIdNotFound() {
+        when(productRepository.findById(productUpdateRequestDto.getId())).thenReturn(Optional.ofNullable(null));
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () -> productServiceImpl.updateProduct(productUpdateRequestDto));
+        assertThat(notFoundException.getMessage()).isEqualTo("Product not found");
+    }
+
+    @Test
+    public void updateProduct_ShouldThrownConflictException_WhenFindProductByNameExitsAndProductOldNotEqualProductExits() {
+        Product productOld = Product.builder().id(1L).build();
+        Product productExist = Product.builder().id(2L).build();
+        when(productRepository.findById(productUpdateRequestDto.getId())).thenReturn(Optional.ofNullable(productOld));
+        when(productRepository.findByName(productUpdateRequestDto.getName())).thenReturn(Optional.ofNullable(productExist));
+        ConflictException conflictException = assertThrows(ConflictException.class, () -> productServiceImpl.updateProduct(productUpdateRequestDto));
+        assertThat(conflictException.getMessage()).isEqualTo("Product name already exits");
+    }
+
+    @Test
+    public void updateProduct_ShouldThrownNotFoundException_WhenFindCategoryByIdNotFound() {
+        Product productOld = mock(Product.class);
+        Product productExist = mock(Product.class);
+        when(productRepository.findById(productUpdateRequestDto.getId())).thenReturn(Optional.ofNullable(productOld));
+        when(productRepository.findByName(productUpdateRequestDto.getName())).thenReturn(Optional.ofNullable(productExist));
+        when(modelMapper.map(productUpdateRequestDto, Product.class)).thenReturn(productInitial);
+        when(categoryRepository.findById(productUpdateRequestDto.getCategoryId())).thenReturn(Optional.ofNullable(null));
+        NotFoundException notFoundException = assertThrows(NotFoundException.class, () -> productServiceImpl.updateProduct(productUpdateRequestDto));
+        assertThat(notFoundException.getMessage()).isEqualTo("Category not found");
+    }
+
+    @Test
+    public void updateProduct_ShouldProductResponseDto_WhenUpdateSuccess() {
+        Product productOld = mock(Product.class);
+        Product productExist = mock(Product.class);
+        when(productRepository.findById(productUpdateRequestDto.getId())).thenReturn(Optional.ofNullable(productOld));
+        when(productRepository.findByName(productUpdateRequestDto.getName())).thenReturn(Optional.ofNullable(productExist));
+        when(modelMapper.map(productUpdateRequestDto, Product.class)).thenReturn(productInitial);
+        when(categoryRepository.findById(productUpdateRequestDto.getCategoryId())).thenReturn(Optional.ofNullable(category));
+        when(productRepository.save(productInitial)).thenReturn(productExpected);
+        when(modelMapper.map(productExpected, ProductResponseDto.class)).thenReturn(productResponseDto);
+        ProductResponseDto result = productServiceImpl.updateProduct(productUpdateRequestDto);
         assertThat(result).isEqualTo(productResponseDto);
     }
 
