01a42690bdeb6faa2ef5ee5a345de00a53a3a6c0
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-19 14:21:57 +0700

    BE: create UnauthorizedException

--- backend/src/test/java/com/example/ecommercenashtechbackend/configuration/SpringSecurityWebAuxTestConfig.java
+++ backend/src/test/java/com/example/ecommercenashtechbackend/configuration/SpringSecurityWebAuxTestConfig.java
@@ -4,10 +4,14 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 
 @TestConfiguration
 public class SpringSecurityWebAuxTestConfig {
@@ -15,15 +19,10 @@ public class SpringSecurityWebAuxTestConfig {
     @Bean
     @Primary
     public UserDetailsService userDetailsService() {
-        UserActive basicActiveUser = new UserActive("user@gmail.com", "admin", Arrays.asList(
-                new SimpleGrantedAuthority("USER")
-        ));
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        User user = new User("admin", "password", authorities);
 
-        UserActive basicActiveAdmin = new UserActive("admin@gmail.com", "user", Arrays.asList(
-                new SimpleGrantedAuthority("ADMIN")
-        ));
-        return new InMemoryUserDetailsManager(Arrays.asList(
-                basicActiveUser, basicActiveAdmin
-        ));
+        return new InMemoryUserDetailsManager(Arrays.asList(user));
     }
 }
