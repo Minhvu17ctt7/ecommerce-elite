f018349e526eb923cc830825cdf5b753466b93f5
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-20 16:25:40 +0700

    BE: create CartRepository

--- backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartRepository.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartRepository.java
@@ -1,9 +1,14 @@
 package com.example.ecommercenashtechbackend.repository;
 
 import com.example.ecommercenashtechbackend.entity.Cart;
+import com.example.ecommercenashtechbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findCartByUser(User user);
 }
