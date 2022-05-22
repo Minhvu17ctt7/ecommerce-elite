13e89768e9150f74bf03f786187c7af911692286
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-22 20:05:47 +0700

    BE: update db H2 findByName_ShouldReturnOptionalProduct_WhenFindByName in

--- backend/src/test/java/com/example/ecommercenashtechbackend/repository/ProductRepositoryTest.java
+++ backend/src/test/java/com/example/ecommercenashtechbackend/repository/ProductRepositoryTest.java
@@ -1,33 +1,32 @@
 package com.example.ecommercenashtechbackend.repository;
 
 import com.example.ecommercenashtechbackend.entity.Product;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ProductRepositoryTest {
-    @Autowired
-    private TestEntityManager entityManager;
 
     @Autowired
     private ProductRepository productRepository;
 
-    private Product PRODUCT_1;
-    private Product PRODUCT_2;
-    private Product PRODUCT_3;
-
-
     @Test
     public void findByName_ShouldReturnOptionalProduct_WhenFindByName() {
+        Product product = Product.builder().name("Áo Thun Dry Cổ Tròn Ngắn Tay").build();
+        productRepository.save(product);
+
         Optional<Product> productOptional = productRepository.findByName("Áo Thun Dry Cổ Tròn Ngắn Tay");
         assertThat(productOptional.isPresent()).isTrue();
         assertThat(productOptional.get().getName()).isEqualTo("Áo Thun Dry Cổ Tròn Ngắn Tay");
@@ -39,5 +38,15 @@ public class ProductRepositoryTest {
         assertThat(productOptional.isPresent()).isTrue();
         assertThat(productOptional.get().getId()).isEqualTo(1L);
     }
+
+//    @Test
+//    public void findAll_ShouldReturnPageProduct_WhenFindSuccess() {
+//        Sort sort = Sort.by(sortField);
+//        sort = sortName.equals("asc") ? sort.ascending() : sort.descending();
+//        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, sort);
+//        Optional<Product> productOptional = productRepository.(1L);
+//        assertThat(productOptional.isPresent()).isTrue();
+//        assertThat(productOptional.get().getId()).isEqualTo(1L);
+//    }
 
 }
