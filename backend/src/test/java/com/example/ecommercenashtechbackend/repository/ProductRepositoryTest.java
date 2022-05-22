b9ba7a8981d83ad9bb9fad38d86595c80ff5ed94
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-22 20:10:46 +0700

    BE: create findByName_ShouldReturnOptionalEmpty_WhenProductNotFound in ProductRepositoryTest

--- backend/src/test/java/com/example/ecommercenashtechbackend/repository/ProductRepositoryTest.java
+++ backend/src/test/java/com/example/ecommercenashtechbackend/repository/ProductRepositoryTest.java
@@ -42,20 +42,17 @@ public class ProductRepositoryTest {
     }
 
     @Test
+    public void findByName_ShouldReturnOptionalEmpty_WhenProductNotFound() {
+        Optional<Product> productOptional = productRepository.findByName("Áo Thun Dry Cổ Tròn Ngắn Tay");
+        assertThat(productOptional.isEmpty()).isTrue();
+    }
+
+    @Test
     public void findById_ShouldReturnOptionalProduct_WhenFindById() {
         Optional<Product> productOptional = productRepository.findById(1L);
         assertThat(productOptional.isPresent()).isTrue();
         assertThat(productOptional.get().getId()).isEqualTo(1L);
     }
 
-//    @Test
-//    public void findAll_ShouldReturnPageProduct_WhenFindSuccess() {
-//        Sort sort = Sort.by(sortField);
-//        sort = sortName.equals("asc") ? sort.ascending() : sort.descending();
-//        Pageable pageable = PageRequest.of(pageNumber - 1, pageSize, sort);
-//        Optional<Product> productOptional = productRepository.(1L);
-//        assertThat(productOptional.isPresent()).isTrue();
-//        assertThat(productOptional.get().getId()).isEqualTo(1L);
-//    }
 
 }
