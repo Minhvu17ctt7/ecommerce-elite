76d53366672f8888422fcfbbbaed8271534e7717
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-24 13:34:25 +0700

    BE: CartItemRepository - add method deletedCartItem

--- backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartItemRepository.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartItemRepository.java
@@ -3,7 +3,9 @@ package com.example.ecommercenashtechbackend.repository;
 import com.example.ecommercenashtechbackend.entity.CartItem;
 import com.example.ecommercenashtechbackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,7 @@ import java.util.List;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> findAllByProductIn(List<Product> products);
+    @Modifying
+    @Query("delete from CartItem b where b.id=:id")
+    void deleteCartItem(@Param("id") Long id);
 }
