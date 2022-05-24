cf88855deb264188a0b328eca4a90b60a3de0ac6
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-24 11:04:02 +0700

    BE: CartController - create addNewItemToCart

--- backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartItemRepository.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/repository/CartItemRepository.java
@@ -1,9 +1,14 @@
 package com.example.ecommercenashtechbackend.repository;
 
 import com.example.ecommercenashtechbackend.entity.CartItem;
+import com.example.ecommercenashtechbackend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findAllByProductIn(List<Product> products);
 }
