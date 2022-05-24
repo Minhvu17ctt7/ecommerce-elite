c2188ee3d7beadb6e4e39fa31a39691613da0ce7
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-24 16:06:31 +0700

    BE: CartController - add method deleteItemToCart

--- backend/src/main/java/com/example/ecommercenashtechbackend/controller/customer/CartController.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/controller/customer/CartController.java
@@ -1,13 +1,15 @@
 package com.example.ecommercenashtechbackend.controller.customer;
 
-import com.example.ecommercenashtechbackend.dto.request.NewCartItemRequestDto;
-import com.example.ecommercenashtechbackend.dto.response.CartResponseDto;
+import com.example.ecommercenashtechbackend.dto.request.CartItemRequestDto;
+import com.example.ecommercenashtechbackend.dto.request.ValidList;
+import com.example.ecommercenashtechbackend.dto.response.CartItemResponseDto;
 import com.example.ecommercenashtechbackend.dto.response.ResponseDto;
 import com.example.ecommercenashtechbackend.security.UserDetail;
 import com.example.ecommercenashtechbackend.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,14 +24,27 @@ public class CartController {
 
     private final CartService cartService;
 
-    @PostMapping
-    public ResponseEntity< ResponseDto<List<CartResponseDto>>> addNewItemToCart(@RequestBody List<NewCartItemRequestDto> newCartItemRequestDtoList) {
+    @PostMapping("/add")
+    public ResponseEntity< ResponseDto<List<CartItemResponseDto>>> addNewItemToCart(@Validated @RequestBody ValidList<CartItemRequestDto> cartItemRequestDtoList) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         UserDetail userDetail = (UserDetail)principal;
 
-        List<CartResponseDto> cartResponseDto = cartService.changeCartItemsInCart(newCartItemRequestDtoList, userDetail.getUser().getId());
-        ResponseDto<List<CartResponseDto>> responseDto = new ResponseDto<>(200, cartResponseDto, "Add Item to cart successfully");
+        List<CartItemResponseDto> cartItemResponseDto = cartService.changeCartItemsInCart(cartItemRequestDtoList.getList(), userDetail.getUser().getId());
+        ResponseDto<List<CartItemResponseDto>> responseDto = new ResponseDto<>(200, cartItemResponseDto, "Add Item to cart successfully");
         return ResponseEntity.ok(responseDto);
     }
+
+    @PostMapping("/delete")
+    public ResponseEntity< ResponseDto<List<CartItemResponseDto>>> deleteItemToCart(@Validated @RequestBody ValidList<CartItemRequestDto> cartItemRequestDtoList) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetail userDetail = (UserDetail)principal;
+
+        List<CartItemResponseDto> cartItemResponseDto = cartService.deleteItemToCart(cartItemRequestDtoList.getList(), userDetail.getUser().getId());
+        ResponseDto<List<CartItemResponseDto>> responseDto = new ResponseDto<>(200, cartItemResponseDto, "Add Item to cart successfully");
+        return ResponseEntity.ok(responseDto);
+    }
+
+
+
 
 }
