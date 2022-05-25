1c8bda6b8b0459f2d1fc6e4df3b22548ee3b38ad
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-25 17:23:45 +0700

    BE: OrderRequestDto - create

---
+++ backend/src/main/java/com/example/ecommercenashtechbackend/dto/request/OrderRequestDto.java
@@ -0,0 +1,28 @@
+package com.example.ecommercenashtechbackend.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderRequestDto {
+
+    @NotBlank( message = "Phone is required")
+    private String phone;
+    @NotBlank(message = "First name is required ")
+    private String firstName;
+    @NotBlank(message = "Last name is required ")
+    private String lastName;
+    @NotBlank(message = "Address is required ")
+    private String address;
+    private String city;
+    @NotBlank(message = "Country is required ")
+    private String country;
+    private List<Long> cartItemIdList;
+
+}
