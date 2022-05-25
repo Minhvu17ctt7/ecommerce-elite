4b0970f4a3e70da2bea519b1f29823e0770bd234
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-25 17:19:51 +0700

    BE: Order - delete field not necessary

--- backend/src/main/java/com/example/ecommercenashtechbackend/entity/Order.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/entity/Order.java
@@ -20,29 +20,24 @@ public class Order extends Auditable<String>{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Double total;
-    private String paymentMethod;
-    private String paymentStatus;
-    private String paymentDate;
+    private Double totalPrice = 0d;
+    private Long totalItem = 0l;
     private String address;
-    private String zip;
     private String phone;
-    private String email;
-    private String address2;
-    private String state;
-    private Date deliveryDate;
     private String firstName;
     private String lastName;
     private String city;
     private String country;
-    private String status;
+    private String status = "Start";
 
     @ManyToOne
     @JoinColumn(name = "userId")
     @JsonIgnore
     private User user;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany( cascade = CascadeType.ALL)
+    @JoinColumn(name = "orderId")
     @JsonIgnore
     private Set<OrderItem> orderItems = new HashSet<>();
+
 }
