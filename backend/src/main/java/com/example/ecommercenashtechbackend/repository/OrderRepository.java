611768e0c02ed21ca9cb84254cdd94863d11cf09
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-26 11:19:42 +0700

    BE: OrderRepository - create findOrderByIdAndUserAndDeleted

--- backend/src/main/java/com/example/ecommercenashtechbackend/repository/OrderRepository.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/repository/OrderRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findAllByUserAndDeleted(User user, boolean deleted, Pageable pageable);
+    Optional<Order> findOrderByIdAndUserAndDeleted(Long id, User user, boolean deleted);
 }
