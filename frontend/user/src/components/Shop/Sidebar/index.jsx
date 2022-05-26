c57e28726ef141e2435063f64050cd88f802501c
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-26 17:32:10 +0700

    FE USER: SidebarComponent ShopPage - add handleFilterPrice

--- frontend/user/src/components/Shop/Sidebar/index.jsx
+++ frontend/user/src/components/Shop/Sidebar/index.jsx
@@ -3,16 +3,21 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Sidebar = ({ categories }) => {
     const [searchParams] = useSearchParams();
-    const page = searchParams.get("page") || 1;
     const categoryId = searchParams.get("categoryId");
     const search = searchParams.get("search") || "";
     const sortName = searchParams.get("sortName") || "";
     const sortField = searchParams.get("sortField") || "";
+    const priceFrom = searchParams.get("priceFrom") || "";
+    const priceTo = searchParams.get("priceTo") || "";
     const navigation = useNavigate();
 
-    console.log("categoryid: ", categoryId == "");
     const handleFilterCategory = (id) => {
-        let url = `/shop?page=1&sortField=${sortField}&sortName=${sortName}&categoryId=${id}&search=${search}`;
+        let url = `/shop?page=1&sortField=${sortField}&sortName=${sortName}&categoryId=${id}&priceFrom=${priceFrom}&priceTo=${priceTo}&search=${search}`;
+        navigation(url);
+    }
+
+    const handleFilterPrice = (from, to) => {
+        let url = `/shop?page=1&sortField=${sortField}&sortName=${sortName}&categoryId=${categoryId}&priceFrom=${from}&priceTo=${to}&search=${search}`;
         navigation(url);
     }
 
@@ -48,34 +53,34 @@ const Sidebar = ({ categories }) => {
             {/* Color End */}
 
             {/* Price Start */}
-            {/* <div className="border-bottom mb-4 pb-4">
+            <div className="border-bottom mb-4 pb-4">
                 <h5 className="font-weight-semi-bold mb-4">Filter by price</h5>
                 <form>
                     <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                        <input type="radio" className="custom-control-input" defaultChecked id="price-all" value="" />
-                        <label className="custom-control-label" htmlFor="price-all">All Price</label>
+                        <input type="checkbox" className="custom-control-input" defaultChecked id="price-all" value="" onClick={() => handleFilterPrice("", "")} checked={priceFrom == ""} />
+                        <label className="custom-control-label" htmlFor="price-all" >All Price</label>
                     </div>
                     <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                        <input type="radio" className="custom-control-input" id="price-1" value="price>0;price<100000" />
-                        <label className="custom-control-label" htmlFor="price-1">0 - 100.000 VND</label>
+                        <input type="checkbox" className="custom-control-input" id="price-1" value="price>0;price<100000" onClick={() => handleFilterPrice(0, 100000)} checked={priceFrom == 0 && priceTo == 100000} />
+                        <label className="custom-control-label" htmlFor="price-1" >0 - 100.000 VND</label>
                     </div>
                     <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                        <input type="radio" className="custom-control-input" id="price-2" value="price>100000;price<200000" />
-                        <label className="custom-control-label" htmlFor="price-2">100.000 - 200.000 VND</label>
+                        <input type="checkbox" className="custom-control-input" id="price-2" value="price>100000;price<200000" onClick={() => handleFilterPrice(100000, 200000)} checked={priceFrom == 100000 && priceTo == 200000} />
+                        <label className="custom-control-label" htmlFor="price-2" >100.000 - 200.000 VND</label>
                     </div>
                     <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                        <input type="radio" className="custom-control-input" id="price-3" value="price>200000;price<400000" />
-                        <label className="custom-control-label" htmlFor="price-3">200.000 - 400.000</label>
+                        <input type="checkbox" className="custom-control-input" id="price-3" value="price>200000;price<400000" onClick={() => handleFilterPrice(200000, 400000)} checked={priceFrom == 200000 && priceTo == 400000} />
+                        <label className="custom-control-label" htmlFor="price-3" >200.000 - 400.000</label>
                     </div>
                     <div className="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
-                        <input type="radio" className="custom-control-input" id="price-4" value="price>400000;price<500000" />
-                        <label className="custom-control-label" htmlFor="price-4">400.000 - 500.000 VND</label>
+                        <input type="checkbox" className="custom-control-input" id="price-4" value="price>400000;price<500000" onClick={() => handleFilterPrice(400000, 500000)} checked={priceFrom == 400000 && priceTo == 500000} />
+                        <label className="custom-control-label" htmlFor="price-4" >400.000 - 500.000 VND</label>
                     </div>
                 </form>
-            </div> */}
+            </div>
             {/* Price End */}
 
-        </div >
+        </div>
     );
 }
 
