670df6930b9014e0e8b8c4d23aacf9dfcd1a958d
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-26 09:40:59 +0700

    BE: GlobalConfiguration - add orderItemToOrderItemResponseDtoConverter and cartItemToOrderItemConverter in modelMapper

--- backend/src/main/java/com/example/ecommercenashtechbackend/config/GlobalConfiguration.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/config/GlobalConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.ecommercenashtechbackend.config;
 
 import com.example.ecommercenashtechbackend.dto.response.CartItemResponseDto;
+import com.example.ecommercenashtechbackend.dto.response.OrderItemResponseDto;
 import com.example.ecommercenashtechbackend.dto.response.ProductResponseDto;
 import com.example.ecommercenashtechbackend.entity.Cart;
 import com.example.ecommercenashtechbackend.entity.CartItem;
+import com.example.ecommercenashtechbackend.entity.OrderItem;
 import com.example.ecommercenashtechbackend.entity.Product;
 import com.example.ecommercenashtechbackend.repository.CartRepository;
 import org.modelmapper.Converter;
@@ -36,7 +38,7 @@ public class GlobalConfiguration {
         Converter<?, ?> convertNull =
                 context -> context.getSource() == null ? context.getDestination() : context.getSource();
 
-        Converter<CartItem, CartItemResponseDto> cartItemConverter = new Converter<CartItem, CartItemResponseDto>()
+        Converter<CartItem, CartItemResponseDto> cartItemToCartItemResponseDtoConverter = new Converter<CartItem, CartItemResponseDto>()
         {
             public CartItemResponseDto convert(MappingContext<CartItem, CartItemResponseDto> context)
             {
@@ -54,7 +56,41 @@ public class GlobalConfiguration {
             }
         };
 
-        modelMapper.addConverter(cartItemConverter);
+        Converter<OrderItem, OrderItemResponseDto> orderItemToOrderItemResponseDtoConverter = new Converter<OrderItem, OrderItemResponseDto>()
+        {
+            public OrderItemResponseDto convert(MappingContext<OrderItem, OrderItemResponseDto> context)
+            {
+                OrderItem orderItem = context.getSource();
+                OrderItemResponseDto orderItemResponseDto = new OrderItemResponseDto();
+
+                orderItemResponseDto.setId(orderItem.getId());
+                orderItemResponseDto.setQuantity(orderItem.getQuantity());
+                orderItemResponseDto.setProductId(orderItem.getProduct().getId());
+                orderItemResponseDto.setNameProduct(orderItem.getProduct().getName());
+                orderItemResponseDto.setPriceProduct(orderItem.getProduct().getPrice());
+                orderItemResponseDto.setMainImage(orderItem.getProduct().getMainImage());
+
+                return orderItemResponseDto;
+            }
+        };
+
+        Converter<CartItem, OrderItem> cartItemToOrderItemConverter = new Converter<CartItem, OrderItem>()
+        {
+            public OrderItem convert(MappingContext<CartItem, OrderItem> context)
+            {
+                CartItem cartItem = context.getSource();
+                OrderItem orderItem = new OrderItem();
+
+                orderItem.setQuantity(cartItem.getQuantity());
+                orderItem.setProduct(cartItem.getProduct());
+
+                return orderItem;
+            }
+        };
+
+        modelMapper.addConverter(cartItemToCartItemResponseDtoConverter);
+        modelMapper.addConverter(orderItemToOrderItemResponseDtoConverter);
+        modelMapper.addConverter(cartItemToOrderItemConverter);
         modelMapper.addConverter(convertNull);
         return modelMapper;
     }
