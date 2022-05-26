a83573073d6f77d8a2d2695ee0e208ea52db8bff
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-26 10:55:22 +0700

    BE: OrderController - create api getListOrderPagination

--- backend/src/main/java/com/example/ecommercenashtechbackend/controller/customer/OrderController.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/controller/customer/OrderController.java
@@ -2,6 +2,8 @@ package com.example.ecommercenashtechbackend.controller.customer;
 
 import com.example.ecommercenashtechbackend.dto.request.OrderRequestDto;
 import com.example.ecommercenashtechbackend.dto.response.OrderResponseDto;
+import com.example.ecommercenashtechbackend.dto.response.PaginationResponseDto;
+import com.example.ecommercenashtechbackend.dto.response.ProductPaginationResponseDto;
 import com.example.ecommercenashtechbackend.dto.response.ResponseDto;
 import com.example.ecommercenashtechbackend.security.UserDetail;
 import com.example.ecommercenashtechbackend.service.OrderService;
@@ -9,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,6 +27,16 @@ public class OrderController {
 
         OrderResponseDto orderResponseDto = orderService.createOrder(orderRequestDto, userDetail.getUser().getId());
         ResponseDto<OrderResponseDto> responseDto = new ResponseDto<>(200, orderResponseDto, "Create order successfully");
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/page/{page}")
+    public ResponseEntity<ResponseDto<PaginationResponseDto<OrderResponseDto>>> getListOrderPagination(@PathVariable("page") int pageNumber, @RequestParam int pageSize) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserDetail userDetail = (UserDetail)principal;
+
+        PaginationResponseDto<OrderResponseDto> orderPagination = orderService.getListOrderPagination(userDetail.getUser().getId(), pageNumber, pageSize, false);
+        ResponseDto<PaginationResponseDto<OrderResponseDto>> responseDto = new ResponseDto<>(200, orderPagination, "Get list order successfully");
         return ResponseEntity.ok(responseDto);
     }
 }
