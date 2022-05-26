b0022066248ad5fb7ca4d439239da734ea9cc358
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-26 10:55:43 +0700

    BE: OrderItem - add fetch = FetchType.LAZY

--- backend/src/main/java/com/example/ecommercenashtechbackend/entity/OrderItem.java
+++ backend/src/main/java/com/example/ecommercenashtechbackend/entity/OrderItem.java
@@ -24,7 +24,7 @@ public class OrderItem extends Auditable<String>{
     @JsonIgnore
     private Product product;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "orderId")
     @JsonIgnore
     private Order order;
