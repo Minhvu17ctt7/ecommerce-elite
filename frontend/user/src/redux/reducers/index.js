e31cebc9a17d50710dda908f90b21fcecb24891d
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-27 14:08:55 +0700

    FE USER: redux - add redux solve cart item

--- frontend/user/src/redux/reducers/index.js
+++ frontend/user/src/redux/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import register from './registerReducer';
 import login from './loginReducer';
 import toast from './toastReducer'
+import cart from './cartReducer';
 
 const rootReducer = combineReducers({
-  register, login, toast
+  register, login, toast, cart
 });
 
 export default rootReducer;
--- frontend/user/src/redux/sagas/watchers.js
+++ frontend/user/src/redux/sagas/watchers.js
@@ -2,9 +2,12 @@ import { takeLatest } from 'redux-saga/effects';
 import { registerSaga, loginSaga } from './authenticationSaga';
 
 import * as types from '../actions';
+import { addToCartSaga, removeFromCartSaga } from './cartSaga';
 
 
 export default function* watchUserAuthentication() {
+  yield takeLatest(types.ADD_TO_CART, addToCartSaga);
+  yield takeLatest(types.REMOVE_FROM_CART, removeFromCartSaga);
   yield takeLatest(types.REGISTER_USER, registerSaga);
   yield takeLatest(types.LOGIN_USER, loginSaga);
 }
