8589627a667177aee45d9b22c917ddca1903cd53
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-27 14:12:38 +0700

    FE USER: App - add route /cart

--- frontend/user/src/App.jsx
+++ frontend/user/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Layout/Footer'
 import Topbar from './components/Layout/Topbar'
+import Cart from './pages/Cart'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
@@ -19,6 +20,7 @@ function App() {
           <Route path='/shop' element={<Shop />} />
           <Route path='/products/:id' element={<ProductDetail />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/cart' element={<Cart />} />
           <Route path='/register' element={<Register />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
