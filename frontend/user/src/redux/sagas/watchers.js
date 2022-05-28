3c17e15fc464421418850faad922bcc8dbb40649
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-05-28 17:13:26 +0700

    FE USER: watchers saga - update GET_CART to GET_CART_LOGGED

--- frontend/user/src/redux/sagas/watchers.js
+++ frontend/user/src/redux/sagas/watchers.js
@@ -7,7 +7,7 @@ import { addToCartSaga, getCartSaga, removeFromCartSaga } from './cartSaga';
 
 export default function* watchUserAuthentication() {
   yield takeLatest(types.ADD_TO_CART, addToCartSaga);
-  yield takeLatest(types.GET_CART, getCartSaga);
+  yield takeLatest(types.GET_CART_LOGGED, getCartSaga);
   yield takeLatest(types.REMOVE_FROM_CART, removeFromCartSaga);
   yield takeLatest(types.REGISTER_USER, registerSaga);
   yield takeLatest(types.LOGIN_USER, loginSaga);
